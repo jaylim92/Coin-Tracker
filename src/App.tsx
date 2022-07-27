122a370a863de7d3de3b4dd75ca1dd45a4342459
jaylim92 <90398720+jaylim92@users.noreply.github.com>
2022-07-27 13:09:44 +0900

    useState on TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import React from "react";
-import Circle from "./Circle";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(value);
+    event.currentTarget.reset();
+  };
+
   return (
     <div>
-      <Circle borderColor="yellow" bgColor="teal" />
-      <Circle text="Good" bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input type="text" placeholder="name" onChange={onChange} />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
