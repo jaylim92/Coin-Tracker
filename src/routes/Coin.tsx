467973e2228c9daeb6b61d5e4ee766fa0bf864af
jaylim92 <jaylim3272@gmail.com>
2022-07-31 23:45:13 +0900

    style coin component

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Routes, useLocation, useParams } from "react-router-dom";
+import { Routes, Route, useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
+import Chart from "./Chart";
+import Price from "./Price";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -13,6 +15,7 @@ const Header = styled.header`
   display: flex;
   align-items: center;
   justify-content: center;
+  margin-bottom: 120px;
 `;
 
 const Title = styled.h1`
@@ -23,6 +26,43 @@ const Title = styled.h1`
 const Loader = styled.div`
   text-align: center;
   display: block;
+`;
+
+const ContentBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 480px;
+  height: 9vh;
+  background-color: #222020;
+  border-radius: 15px;
+`;
+const Contents = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  span {
+    &:first-child {
+      font-size: 15px;
+      font-weight: 600;
+      margin-bottom: 5px;
+    }
+    &:last-child {
+      font-size: 25px;
+    }
+  }
+`;
+
+const Description = styled.div`
+  text-align: center;
+  margin: 50px 0px;
+`;
+
+const Img = styled.img`
+  width: 20px;
+  height: 20px;
+  margin-right: 10px;
 `;
 
 interface RouteParams {
@@ -107,15 +147,57 @@ function Coin() {
       console.log(priceData);
       setCoinInfo(infoData);
       setPriceInfo(priceData);
+      setLoading(false);
     })();
-  }, []);
+  }, [coinId]);
 
   return (
     <Container>
       <Header>
-        <Title>{state?.name || "Loading.."}</Title>
+        <Title>
+          {state?.name ? state.name : loading ? "Loading..." : coinInfo?.name}
+        </Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : priceInfo?.quotes.USD.price}
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <ContentBox>
+            <Contents>
+              <span>Rank:</span>
+              <span>{priceInfo?.rank}</span>
+            </Contents>
+            <Contents>
+              <span>SYMBOL:</span>
+              <span>
+                <Img
+                  src={`https://coinicons-api.vercel.app/api/icon/${coinInfo?.symbol.toLowerCase()}`}
+                />
+                {priceInfo?.symbol}
+              </span>
+            </Contents>
+            <Contents>
+              <span>Open Source:</span>
+              <span>{coinInfo?.open_source ? "Yes" : "No"}</span>
+            </Contents>
+          </ContentBox>
+          <Description>{coinInfo?.description}</Description>
+          <ContentBox>
+            <Contents>
+              <span>Total Supply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </Contents>
+            <Contents>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </Contents>
+          </ContentBox>
+          <Routes>
+            <Route path="chart" element={<Chart />} />
+            <Route path="price" element={<Price />} />
+          </Routes>
+        </>
+      )}
     </Container>
   );
 }
